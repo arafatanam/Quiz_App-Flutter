0b68c9974ee8b334e4dddacc80743d19b9931cac
arafatanam <arafatanam01@gmail.com>
2025-10-05 14:57:45 +0600

    Refactor AnswerButton to accept onTap callback and update QuestionsScreen to use it for button actions

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton(this.answerText, {super.key});
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
 
   final String answerText;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () {}, child: Text(answerText));
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      ),
+      child: Text(answerText),
+    );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -19,6 +20,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         children: [
           const Text('The questions!', style: TextStyle(color: Colors.white)),
           const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          AnswerButton(answerText: 'Answer 3', onTap: () {}),
         ],
       ),
     );
