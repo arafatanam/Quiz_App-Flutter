129559c2521578066392a3ffa4fdeda0b0c57258
arafatanam <arafatanam01@gmail.com>
2025-10-24 17:28:25 +0600

    Update Quiz state management to transition to ResultsScreen after answering all questions

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Container(margin: const EdgeInsets.all(40)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const [
-          Text('You did it!'),
-          SizedBox(height: 30),
-          Text('You answered 3 out of 5 questions correctly!'),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You answered 3 out of 5 questions correctly!'),
+            const SizedBox(height: 30),
+            const Text('List of answers and questions...'),
+            const SizedBox(height: 30),
+            TextButton(onPressed: () {}, child: Text('Restart Quiz!')),
+          ],
+        ),
       ),
     );
   }
