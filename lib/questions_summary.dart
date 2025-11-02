6f6729f3da5b7a141eaa11afb9620ee24ecf5263
arafatanam <arafatanam01@gmail.com>
2025-11-02 10:48:19 +0600

    Refactor QuestionsSummary and SummaryItem for improved structure and readability

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
+import 'summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
-  QuestionsSummary(this.summaryData, {super.key});
+  const QuestionsSummary(this.summaryData, {super.key});
 
   final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 400,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            return Row(
-              children: [
-                Text(((data['question_index'] as int) + 1).toString()),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      const SizedBox(height: 5),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
-                    ],
-                  ),
-                ),
-              ],
-            );
+            return SummaryItem(data);
           }).toList(),
         ),
       ),
