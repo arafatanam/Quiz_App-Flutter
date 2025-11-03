65c4e7ae4176ee21845da2aa82fbc4043900a5c2
arafatanam <arafatanam01@gmail.com>
2025-11-03 11:08:16 +0600

    Add restart functionality and improve ResultsScreen UI
    - Implemented restartQuiz method to reset selected answers and switch to questions screen.
    - Updated ResultsScreen to accept onRestart callback for restarting the quiz.
    - Enhanced UI of ResultsScreen with styled text and restart button.
    - Refactored SummaryItem layout for better readability.

--- lib/summary_item.dart
+++ lib/summary_item.dart
@@ -12,42 +12,45 @@ class SummaryItem extends StatelessWidget {
     final isCorrectAnswer =
         itemData['user_answer'] == itemData['correct_answer'];
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        QuestionIdentifier(
-          isCorrectAnswer: isCorrectAnswer,
-          questionIndex: itemData['question_index'] as int,
-        ),
-        const SizedBox(width: 20),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                itemData['question_text'] as String,
-                style: GoogleFonts.lato(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                itemData['user_answer'] as String,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 202, 107, 252),
-                ),
-              ),
-              Text(
-                itemData['correct_answer'] as String,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 181, 254, 246),
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuestionIdentifier(
+            isCorrectAnswer: isCorrectAnswer,
+            questionIndex: itemData['question_index'] as int,
           ),
-        ),
-      ],
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemData['question_text'] as String,
+                  style: GoogleFonts.lato(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  itemData['user_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 202, 107, 252),
+                  ),
+                ),
+                Text(
+                  itemData['correct_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 181, 254, 246),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
